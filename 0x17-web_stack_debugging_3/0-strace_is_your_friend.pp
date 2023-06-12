bb2ae4dc0b0fa52954917e6b047c1b4a3214e89c
Nobleman20 <emmanueloluyege@gmail.com>
2023-06-12 03:30:58 +0000

    Commit1

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# Correct wrongly named `phpp` extensions to `php`
+
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
